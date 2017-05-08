c8c5c4a01d6d91ea1b7d706e1c7aad2df42f4066
Tristan Jay Mahr <Mahr@Wisc.Edu>
2017-05-08 09:19:19 -0500

    pass package checks

--- R/tristan-package.R
+++ R/tristan-package.R
@@ -5,8 +5,71 @@
 NULL
 
 
-#' Create a long data-frame of linear-posterior predictions
+#' Create a long data-frame of posterior predictions
+#'
+#'
+#' These functions are wrapper around RStanARM's prediction functions that
+#' return a long tidy dataframe of model predictions.
+#' `augment_posterior_predict()` wraps [rstanarm::posterior_predict()] and
+#' `augment_posterior_linpred()` wraps [rstanarm::posterior_predict()].
+#'
+#' @param model a model fit with [RStanARM][rstanarm::rstanarm]
+#' @param newdata a dataframe of new observations to generate predictions. If
+#'   omitted or null, the original data-set used to fit the model is used for
+#'   the predictions.
+#' @param ... other arguments passed to [rstanarm::posterior_predict()] or
+#' [rstanarm::posterior_linpred()].
+#' @param nsamples number of samples to draw from the posterior. Defaults to the
+#'   full number of samples.
+#' @return a data-frame (a [tibble::tibble()]) of posterior predictions. The
+#'   dataset used to generate the predictions is augmented with posterior
+#'   predictions, so that there is row per posterior prediction per observation.
+#'   Additionaly columns are included: `.observation` uniquely identifies rows
+#'   from the prediction data-set, `.draw` is the posterior sample number, and
+#'   `.posterior_value` is the predicted value for that `.observation` in that
+#'   `.draw`.
+#' @name augment_posterior
+NULL
+
+
+
 #' @export
+#' @rdname augment_posterior
+augment_posterior_predict <- function(model, newdata = NULL, ..., nsamples = NULL) {
+  if (is.null(newdata)) {
+    newdata <- model$data
+  }
+
+  # Number the observations in the data
+  newdata[[".observation"]] <- seq_len(nrow(newdata))
+
+  # Figure out which observations the model will handle by adding the
+  # observation ID to the model formula
+  temp_formula <- modelr::add_predictors(stats::formula(model), ~ .observation)
+  # Outcome variable not required
+  temp_formula[2] <- NULL
+  model_ready_data <- stats::model.frame(temp_formula, newdata)
+
+  # Keep only obersvations that the model can handle
+  preddata <- newdata[newdata$.observation %in% model_ready_data$.observation, ]
+
+  # Do the predictions
+  preds <- rstanarm::posterior_predict(model, newdata = preddata,
+                                       draws = nsamples, ...)
+  long_preds <- reshape2::melt(
+    data = preds,
+    varnames = c(".draw", ".observation"),
+    value.name = ".posterior_value")
+
+  long_preds <- long_preds[c(".observation", ".draw", ".posterior_value")]
+
+  long_preds_w_data <- merge(long_preds, preddata, by = ".observation")
+
+  tibble::as.tibble(long_preds_w_data)
+}
+
+#' @export
+#' @rdname augment_posterior
 augment_posterior_linpred <- function(model, newdata = NULL, ..., nsamples = NULL) {
   if (is.null(newdata)) {
     newdata <- model$data
@@ -17,7 +80,7 @@ augment_posterior_linpred <- function(model, newdata = NULL, ..., nsamples = NUL
 
   # Figure out which observations the model will handle by adding the
   # observation ID to the model formula
-  temp_formula <- modelr::add_predictors(formula(model), ~ .observation)
+  temp_formula <- modelr::add_predictors(stats::formula(model), ~ .observation)
   # Outcome variable not required
   temp_formula[2] <- NULL
   model_ready_data <- stats::model.frame(temp_formula, newdata)
@@ -49,47 +112,14 @@ augment_posterior_linpred <- function(model, newdata = NULL, ..., nsamples = NUL
 
 
 
-
-
-
-#' Create a long data-frame of posterior predictions
-#' @export
-augment_posterior_predict <- function(model, newdata = NULL, ..., nsamples = NULL) {
-  if (is.null(newdata)) {
-    newdata <- model$data
-  }
-
-  # Number the observations in the data
-  newdata[[".observation"]] <- seq_len(nrow(newdata))
-
-  # Figure out which observations the model will handle by adding the
-  # observation ID to the model formula
-  temp_formula <- modelr::add_predictors(formula(model), ~ .observation)
-  # Outcome variable not required
-  temp_formula[2] <- NULL
-  model_ready_data <- stats::model.frame(temp_formula, newdata)
-
-  # Keep only obersvations that the model can handle
-  preddata <- newdata[newdata$.observation %in% model_ready_data$.observation, ]
-
-  # Do the predictions
-  preds <- rstanarm::posterior_predict(model, newdata = preddata,
-                                       draws = nsamples, ...)
-  long_preds <- reshape2::melt(
-    data = preds,
-    varnames = c(".draw", ".observation"),
-    value.name = ".posterior_value")
-
-  long_preds <- long_preds[c(".observation", ".draw", ".posterior_value")]
-
-  long_preds_w_data <- merge(long_preds, preddata, by = ".observation")
-
-  tibble::as.tibble(long_preds_w_data)
-}
-
-
+#' Create tidy dataframe for ggmcmc from an RStanARM model
+#'
+#' @param model a model fit with [RStanARM][rstanarm::rstanarm]
+#' @return a data-frame (a [tibble::tibble()]) of MCMC sampling information
+#'   suitable for use with the [ggmcmc package][ggmcmc::ggmcmc].
 #' @export
 ggs_rstanarm <- function(model) {
+  stopifnot(inherits(model, "stanreg"))
   chains <- as.array(model)
   dimnames(chains)[["chains"]] <- seq_along(dimnames(chains)[["chains"]])
   long <- reshape2::melt(chains, c("Iteration", "Chain", "Parameter"), "value")
